e22b30cd3c66e12a35e8b3d0ef0a641702cca950
Arun Tekam <aruntekam812@gmail.com>
2026-06-09 14:12:43 +0530

    Delete Python directory

--- Python/basic+ Funnctions+loops.py/4.if-else.py
+++
@@ -1,8 +0,0 @@
-a =int(input("enter your age:"))
-print("my age is:",a)
-if(a>18):
-    print("you are young man")
-else:
-    print("you are a boy")
-
-
--- Python/oop.py/21.generators .py
+++
@@ -1,8 +0,0 @@
-def generator():
-    for i in range(5):
-        yield i
-   
-
-gen = generator()
-for j in gen:
-    print(j)
